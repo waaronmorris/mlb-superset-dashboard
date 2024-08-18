d7767c84491c2fa4dc3204d11dc55b8e261b4ee9
W. Aaron Morris <waaronmorris@gmail.com>
2024-08-17 20:06:59 -0500

    feat: MDS in a Box

---
+++ mlb_stat_duckdb/models/example/box_scores.sql
@@ -0,0 +1,13 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+Select *
+FROM {{ source("mlb_stats","mlb_box_scores")}}
